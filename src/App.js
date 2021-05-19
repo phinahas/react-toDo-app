4e01f9433849a186a526b98f20bbe4d12fa9ae6b
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-05-19 12:18:00 +0530

    Each jobs added to todo array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const[toDos,setToDos]=useState([])
+  const[toDo,setTodo]=useState('')
+
+
+const addToDo=()=>{
+  setToDos([...toDos,toDo])
+}
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,20 +21,27 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=>{setTodo(e.target.value)}} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={addToDo} className="fas fa-plus"></i>
       </div>
-      <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
+
+      {toDos.map((jobs)=>{
+          return(
+          <div className="todos">
+          <div className="todo">
+            <div className="left">
+              <input type="checkbox" name="" id="" />
+              <p>{jobs}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
           </div>
         </div>
-      </div>
+
+         ) })
+      }
+     
     </div>
   );
 }
