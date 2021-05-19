6c8a3a35c446c317b07a4424745feef4fd187bf4
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-05-19 15:49:07 +0530

    Deletion and mark as done for the  todo list copleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
 
   const[toDos,setToDos]=useState([])
   const[toDo,setTodo]=useState('')
+  const[today,setDay]=useState('')
 
+  useEffect(()=>{
+    console.log("Use effect running");
+    day();
+//console.log(today.getDay());
+  },[])
+
+
+const day =()=>{
+  console.log("function running");
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var today = new Date();
+  var dayName = days[today.getDay()];
+  setDay(dayName)
+
+}
 
 const addToDo=()=>{
-  setToDos([...toDos,toDo])
+  setToDos([...toDos,{id:Date.now(),text:toDo,done:false,deleted:false}])
+  setTodo('')
 }
+
+const showToDo=()=>{
+  console.log(toDos);
+}
+
 
   return (
     <div className="app">
       <div className="mainHeading">
-        <h1>ToDo List</h1>
+        <h1 onClick={showToDo}>ToDo List</h1>
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's {today} 🌝 ☕ </h2>
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>{setTodo(e.target.value)}} type="text" placeholder="🖊️ Add item..." />
@@ -26,20 +48,38 @@ const addToDo=()=>{
       </div>
 
       {toDos.map((jobs)=>{
+        if(jobs.deleted===false)
+        {
           return(
           <div className="todos">
           <div className="todo">
             <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>{jobs}</p>
+              <input type="checkbox" name="" id="" onChange={(e)=>{
+                setToDos(toDos.filter(obj=>{
+                  if(obj.id===jobs.id){
+                    obj.done=!obj.done
+                  }
+                  return obj
+                }))
+              }} />
+              <p>{jobs.text}</p>
             </div>
             <div className="right">
-              <i className="fas fa-times"></i>
+              <i onClick={()=>{
+                 setToDos(toDos.filter(obj=>{
+                  if(obj.id===jobs.id){
+                    obj.deleted=true
+                  }
+                  return obj
+                }))
+              }} className="fas fa-times"></i>
             </div>
           </div>
         </div>
 
-         ) })
+         )}
+        return null
+        })
       }
      
     </div>
